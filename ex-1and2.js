790b45b65551814a5dc66c68df35eaeb7fbecff4
Pom <chayakon500@gmail.com>
2024-08-14 11:33:32 +0700

    Javascript Exam 1-4

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,24 @@
 //Exercise #1: Variable Creation
-//Start coding here
-
+const name = "Karter"
+console.log(name) 
+const surname = "Anderson"
+console.log(surname)
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+console.log(address)
+const nameRestaurant = "Chef's Table by Chef James"
+console.log(nameRestaurant)
+let netPrice = 15000
+console.log(netPrice)
+let openRestaurant = 1500
+console.log(openRestaurant)
 //Exercise #2: Variable Reassignment
-//Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+console.log(address)
+netPrice = 5000
+console.log(netPrice)
+openRestaurant = 2500
+console.log(openRestaurant)
+
+
+
+
--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,8 @@
 // Exercise #3: Celsius to Fahrenheit
 let celsius = 30;
+console.log((celsius*1.8)+32)
 
 // Start coding here;
 
-let fahrenheit;
+let fahrenheit = 86
+console.log(fahrenheit)
